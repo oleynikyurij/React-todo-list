f2c8c6ad19b854744b435b7645d4dda4238ca511
unknown <D:\\Mail>
2018-12-13 13:00:08 +0200

    реализация добавления новой задачи

--- src/components/AddItem/AddItem.js
+++ src/components/AddItem/AddItem.js
@@ -3,16 +3,36 @@ import React, { Component } from 'react';
 import './style.css';
 
 export default class AddItem extends Component {
+    state = {
+        label: ''
+    };
+
+    onLabelChange = event => {
+        console.log(event.target.value);
+        this.setState({
+            label: event.target.value
+        });
+    };
+    onSubmit = event => {
+        event.preventDefault();
+        this.props.onItemAdd(this.state.label);
+        this.setState({
+            label: ''
+        });
+    };
+
     render() {
         return (
-            <div className="item-add-form">
-                <button
-                    className="btn btn-outline-secondary"
-                    onClick={() => this.props.onItemAdd('Hello World')}
-                >
-                    Add Item
-                </button>
-            </div>
+            <form className="item-add-form d-flex" onSubmit={this.onSubmit}>
+                <input
+                    type="text"
+                    className="form-control"
+                    onChange={this.onLabelChange}
+                    placeholder="What needs to be done"
+                    value={this.state.label}
+                />
+                <button className="btn btn-outline-secondary">Add Item</button>
+            </form>
         );
     }
 }
